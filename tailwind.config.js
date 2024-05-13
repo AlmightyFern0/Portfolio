56e201af3234b095830645e8d99ed7707ed531e6
AlmightyFern0 <almightyfern0@gmail.com>
2024-05-14 01:31:42 +0200

    Created Navbar plus animations (not created the page list view)

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,24 @@
 module.exports = {
   content: ["./src/*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'tech-magenta': '#E03BE3',
+        'kinky-purple': '#55286F',
+        'death-gray': '#1F2020',
+      },
+      backgroundSize: {
+        'size-200': '200% 200%',
+      },
+      backgroundPosition: {
+        'pos-0': '0% 0%',
+        'pos-100': '100% 100%',
+      },
+    },
+
+    fontFamily: {
+      'main': ['Sora', 'sans-serif'],
+    },
   },
   plugins: [],
 }
