23804cb1075b18841b671c341240bacd68220043
AlmightyFern0 <almightyfern0@gmail.com>
2024-05-27 23:21:21 +0200

    Fixed the navbar (I still need to create the animations), Started the homepage.
    Fuck I'm slow...

--- src/navbarOpCl.js
+++ src/navbarOpCl.js
@@ -1,9 +1,9 @@
 function toggleMenu() {
     var menuBox = document.getElementById('menu-box');    
-    if(menuBox.style.display == "none") { // if is menuBox displayed, hide it
-      menuBox.style.display = "block";
+    if(menuBox.style.display == "block") { // if is menuBox displayed, hide it
+      menuBox.style.display = "none";
     }
     else { // if is menuBox hidden, display it
-      menuBox.style.display = "none";
+      menuBox.style.display = "block";
     }
   }
